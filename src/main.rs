451ae03351270e470b3a634c530a24e5d65e0665
Nick Pupko <work.pupko@gmail.com>
2023-02-09 20:11:34 +0300

    Add correct money handling

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use serde::{Deserialize, Serialize};
 use clap::Parser;
 use colored::Colorize;
+use rust_decimal::prelude::*;
 
 #[allow(non_snake_case)]
 #[derive(Serialize, Deserialize, Debug)]
@@ -28,7 +29,7 @@ struct Cli {
 async fn main() {
     let args = Cli::parse();
     let date = &args.date;
-    let amount = args.amount;
+    let amount = Decimal::from_f32(args.amount.unwrap_or(0.0));
 
     let url = format!(
         "https://www.nbrb.by/api/exrates/rates/431?ondate={date}",
@@ -37,13 +38,14 @@ async fn main() {
 
     let client = reqwest::Client::new();
     let response = client.get(&url).send().await.unwrap();
-    let mut official_rate: f32 = 0.0;
+    let mut official_rate = Decimal::new(0, 0);
 
     match response.status() {
         reqwest::StatusCode::OK => {
             match response.json::<ExchangeRate>().await {
                 Ok(exchange_rate) => {
-                    official_rate = exchange_rate.Cur_OfficialRate;
+                    official_rate = Decimal::from_f32(exchange_rate.Cur_OfficialRate).unwrap();
+
                 },
                 Err(_) => println!("Error"),
             }
@@ -55,6 +57,8 @@ async fn main() {
     println!("USD rate for {date} is {rate}", date = date.bold().green(), rate = official_rate.to_string().bold().green());
 
     if let Some(amount) = amount {
+       if amount == Decimal::new(0, 0) { return; }
+
        let result = amount * official_rate;
        println!("You received (in BYN): {}", result.to_string().bold().green());
     }
